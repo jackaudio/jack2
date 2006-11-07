13827ab7b1172255d21bb222d5cb896917e7fbd8
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2006-11-07 16:59:29 +0000

    Cleanup
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@1272 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackAPIWrapper.cpp
+++ common/JackAPIWrapper.cpp
@@ -1,6 +1,5 @@
 /*
-Copyright (C) 2001-2003 Paul Davis
-Copyright (C) 2004-2006 Grame
+Copyright (C) 2006 Grame
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
