9a9d28b162d215e01ee2ad3a5d24fefdf0206950
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2007-10-17 14:16:44 +0000

    Cleanup
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@1619 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/jack/ringbuffer.h
+++ common/jack/ringbuffer.h
@@ -16,7 +16,6 @@
   along with this program; if not, write to the Free Software 
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-  $Id: ringbuffer.h,v 1.2.2.1 2006/06/20 14:44:00 letz Exp $
 */
 
 #ifndef _RINGBUFFER_H
