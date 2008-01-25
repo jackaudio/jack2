10126bdb1ef3b4b23e866652ff3791d0c8b7b8c4
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2008-01-25 19:22:35 +0000

    Cleanup
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@1773 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackServerGlobals.cpp
+++ common/JackServerGlobals.cpp
@@ -36,7 +36,6 @@ static char* server_name = NULL;
 namespace Jack
 {
 
-//long JackServerGlobals::fClientCount = 0;
 unsigned int JackServerGlobals::fClientCount = 0;
 JackServer* JackServerGlobals::fServer = NULL;
 
