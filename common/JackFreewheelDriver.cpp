3241fd63d311266c9a0a8070dfd8eaec13e8c3c6
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2008-03-15 21:18:06 +0000

    Cleanup
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@2011 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackFreewheelDriver.cpp
+++ common/JackFreewheelDriver.cpp
@@ -11,7 +11,6 @@
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-JackEngine.cpp
 
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
