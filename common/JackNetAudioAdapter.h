4665934b184de379a61a45a07fa557a68001eaad
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2008-07-02 12:04:42 +0000

    New JackNetAudioAdapter class
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@2568 0c269be4-1314-0410-8aa9-9f06e86f4224

---
+++ common/JackNetAudioAdapter.h
@@ -0,0 +1,47 @@
+/*
+Copyright (C) 2008 Grame
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef __JackNetAudioAdapter__
+#define __JackNetAudioAdapter__
+
+//#include "JackNetTool.h"
+//#include "thread.h"
+#include "jack.h"
+#include <list>
+
+namespace Jack
+{
+
+	class JackNetAudioAdapter
+	{
+		private:
+        
+			jack_client_t* fJackClient;
+			const char* fClientName;
+      
+            static int Process();
+            
+		public:
+        
+			JackNetAudioAdapter();
+			~JackNetAudioAdapter();
+	};
+}
+
+#endif
