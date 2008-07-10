f0349a3d3dedee2a6d3451e203ed86ee57fcfc95
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2008-07-10 09:56:42 +0000

    Cleanup some headers for licence issues.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@2683 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackTools.cpp
+++ common/JackTools.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2001 Paul Davis
+  Copyright (C) 2006-2008 Grame
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
