0a45edf0137f80943b8d28e755e570f0a0a632d0
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2009-01-28 10:17:58 +0000

    Update XCode project for Universal Binary compilation of netjack components.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@3271 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackNetTool.h
+++ common/JackNetTool.h
@@ -23,6 +23,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include "JackPlatformPlug.h"
 #include "types.h"
 #include "transport.h"
+#include <netinet/in.h>
 #include <cmath>
 
 using namespace std;
