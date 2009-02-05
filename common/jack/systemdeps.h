d2aeec4beb987535a8e5d3cde7be9f893b092b87
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2009-02-05 11:31:43 +0000

    Correct systemdeps.h public header licence.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@3293 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/jack/systemdeps.h
+++ common/jack/systemdeps.h
@@ -1,9 +1,9 @@
 /*
-Copyright (C) 2004-2006 Grame
+Copyright (C) 2004-2009 Grame
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
