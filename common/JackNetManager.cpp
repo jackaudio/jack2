849e366f4e85f7ca69b32afc5f41945df9552857
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2009-03-27 14:10:52 +0000

    Add a buffer size callback for netmaster that just remove the client (it will be recreated with the new parameters).
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@3471 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackNetManager.cpp
+++ common/JackNetManager.cpp
@@ -127,7 +127,11 @@ namespace Jack
             return false;
         }
 
-        jack_set_process_callback ( fJackClient, SetProcess, this );
+        if (jack_set_process_callback(fJackClient, SetProcess, this ) < 0)
+             goto fail;
+             
+        if (jack_set_buffer_size_callback(fJackClient, SetBufferSize, this) < 0)
+             goto fail;
 
         if ( AllocPorts() != 0 )
         {
@@ -368,6 +372,13 @@ namespace Jack
     bool JackNetMaster::IsSlaveReadyToRoll()
     {
         return ( fReturnTransportData.fState == JackTransportNetStarting );
+    }
+    
+    int JackNetMaster::SetBufferSize (jack_nframes_t nframes, void* arg)
+    {
+        jack_error("Cannot handle bufer size change, so proxy will be removed...");
+        static_cast<JackNetMaster*> ( arg )->Exit();
+        return 0;
     }
 
 //process-----------------------------------------------------------------------------
