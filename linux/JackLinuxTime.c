a4479183018d3fd0de4416dcf39ca5d8ea80b92e
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2009-04-20 14:05:55 +0000

    Add missing include.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@3509 0c269be4-1314-0410-8aa9-9f06e86f4224

--- linux/JackLinuxTime.c
+++ linux/JackLinuxTime.c
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #endif
 
 #include "JackTime.h"
+#include "JackTypes.h"
 #include <unistd.h>
 
 SERVER_EXPORT void JackSleep(long usec) 
--- windows/JackWinTime.c
+++ windows/JackWinTime.c
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 #include <windows.h>
 #include "JackTime.h"
+#include "JackTypes.h"
 #include "JackError.h"
 
 static LARGE_INTEGER _jack_freq;
