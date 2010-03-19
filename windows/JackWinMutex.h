c811596499644006c2cc38836b2acbb8fb45937c
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2010-03-19 15:35:42 +0000

    Fix some file header to have library side code use LGPL.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@3961 0c269be4-1314-0410-8aa9-9f06e86f4224

--- windows/JackWinMutex.h
+++ windows/JackWinMutex.h
@@ -1,23 +1,22 @@
 /*
- Copyright (C) 2006 Grame
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
-
- This library is distributed in the hope that it will be useful,
+ Copyright (C) 2004-2008 Grame
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 2.1 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ 
+ */
 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
- Grame Research Laboratory, 9 rue du Garet, 69001 Lyon - France
- grame@grame.fr
-*/
 
 #ifndef __JackWinMutex__
 #define __JackWinMutex__
