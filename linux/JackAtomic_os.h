47c307b79037e9b96a403cd60d1d41b60556293f
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2010-04-28 08:56:28 +0000

    Apply patch for ticket #169.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@3994 0c269be4-1314-0410-8aa9-9f06e86f4224

--- linux/JackAtomic_os.h
+++ linux/JackAtomic_os.h
@@ -67,6 +67,15 @@ static inline char CAS(volatile UInt32 value, UInt32 newvalue, volatile void* ad
     return ret;
 }
 
+#else
+
+#warning using builtin gcc (version > 4.1) atomic
+
+static inline char CAS(volatile uint32_t value, uint32_t newvalue, volatile int32_t* addr)
+{
+    return __sync_bool_compare_and_swap (addr, value, newvalue);
+}
+
 #endif
 
 #endif
