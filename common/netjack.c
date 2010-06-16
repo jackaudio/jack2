9b2744c62d31715f6b54320f5b1debe90d333373
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2010-06-16 16:03:08 +0000

    David Garcia Garzon unused_pkt_buf_field_jack2 netone patch.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4024 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/netjack.c
+++ common/netjack.c
@@ -735,7 +735,6 @@ netjack_startup( netjack_driver_state_t *netj )
     }
 
     netj->rx_bufsize = sizeof (jacknet_packet_header) + netj->net_period_down * netj->capture_channels * get_sample_size (netj->bitdepth);
-    netj->pkt_buf = malloc (netj->rx_bufsize);
     global_packcache = packet_cache_new (netj->latency + 50, netj->rx_bufsize, netj->mtu);
 
     netj->expected_framecnt_valid = 0;
