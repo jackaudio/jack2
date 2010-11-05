64551f9bcd5cfc10a133deaeaf52d5641e1a6839
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2010-11-05 09:31:40 +0000

    Correct symbols export issue on OSX.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4080 0c269be4-1314-0410-8aa9-9f06e86f4224

---
+++ macosx/JackCompilerDeps_os.h
@@ -0,0 +1,55 @@
+/*
+Copyright (C) 2004-2005 Grame
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef __JackCompilerDeps_APPLE__
+#define __JackCompilerDeps_APPLE__
+
+#include "JackConstants.h"
+
+#if __GNUC__
+    #ifndef POST_PACKED_STRUCTURE
+        /* POST_PACKED_STRUCTURE needs to be a macro which
+        expands into a compiler directive. The directive must
+        tell the compiler to arrange the preceding structure
+        declaration so that it is packed on byte-boundaries rather
+        than use the natural alignment of the processor and/or
+        compiler.
+        */
+        #if defined(JACK_32_64)
+            #define POST_PACKED_STRUCTURE __attribute__((__packed__))
+        #else
+            #define POST_PACKED_STRUCTURE
+        #endif
+    #endif
+    #define MEM_ALIGN(x,y) x __attribute__((aligned(y)))
+    #define EXPORT __attribute__((visibility("default")))
+    #ifdef SERVER_SIDE
+        #define SERVER_EXPORT __attribute__((visibility("default")))
+    #else
+        #define SERVER_EXPORT 
+    #endif
+#else
+    #define MEM_ALIGN(x,y) x
+    #define EXPORT
+    #define SERVER_EXPORT
+    /* Add other things here for non-gcc platforms for POST_PACKED_STRUCTURE */
+#endif
+
+#endif
+
