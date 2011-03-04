4ad398cc1e6c72f3b7d784b40e0f8689a20d4919
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2011-03-04 12:47:21 +0000

    Oups..
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4149 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackPortType.h
+++ common/JackPortType.h
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #ifndef __JackPortType__
 #define __JackPortType__
 
+#include "types.h"
 #include "JackConstants.h"
 #include <stddef.h>
 
