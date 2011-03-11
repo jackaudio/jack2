e2c25b2fa7e246fd8e8b4d19f2d7b27ce85eb52b
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2011-03-11 10:59:03 +0000

    Works again on Windows.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4177 0c269be4-1314-0410-8aa9-9f06e86f4224

--- windows/portaudio/JackPortAudioDriver.h
+++ windows/portaudio/JackPortAudioDriver.h
@@ -82,6 +82,12 @@ class JackPortAudioDriver : public JackAudioDriver
         int Read();
         int Write();
 
+        // BufferSize can be changed
+        bool IsFixedBufferSize()
+        {
+            return false;
+        }
+
         int SetBufferSize(jack_nframes_t buffer_size);
 };
 
