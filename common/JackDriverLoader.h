db4b99b5b6031fec3d20fc5b880b52d43917ecac
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2011-05-04 09:55:27 +0000

    Fix link issues for jackd.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4377 0c269be4-1314-0410-8aa9-9f06e86f4224

--- common/JackDriverLoader.h
+++ common/JackDriverLoader.h
@@ -59,9 +59,18 @@ jack_driver_desc_t * jack_find_driver_descriptor(JSList * drivers, const char * 
 JSList * jack_drivers_load(JSList * drivers);
 JSList * jack_internals_load(JSList * internals);
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 SERVER_EXPORT int jackctl_parse_driver_params (jackctl_driver * driver_ptr, int argc, char* argv[]);
 SERVER_EXPORT void jack_free_driver_params(JSList * param_ptr);
 SERVER_EXPORT void jack_print_driver_options(jack_driver_desc_t* desc, FILE* file);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
