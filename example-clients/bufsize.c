55516933a914b56f6addc0b1a636243a82eb51e7
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2011-06-14 09:20:20 +0000

    Typos in bufsize.c.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4456 0c269be4-1314-0410-8aa9-9f06e86f4224

--- example-clients/bufsize.c
+++ example-clients/bufsize.c
@@ -71,13 +71,13 @@ void parse_arguments(int argc, char *argv[])
 
 	nframes = strtoul(argv[1], NULL, 0);
 	if (errno == ERANGE) {
-		fprintf(stderr, "%s: invalid buffer size: %s (range is 1-8182)\n",
+		fprintf(stderr, "%s: invalid buffer size: %s (range is 1-8192)\n",
 			package, argv[1]);
 		exit(2);
 	}
 
     if (nframes < 1 || nframes > 8182) {
-		fprintf(stderr, "%s: invalid buffer size: %s (range is 1-8182)\n",
+		fprintf(stderr, "%s: invalid buffer size: %s (range is 1-8192)\n",
 			package, argv[1]);
 		exit(3);
     }
