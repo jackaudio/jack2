311d4ae225e63e0cfce8ee80fcab26774b953a79
Stephane Letz <letz@grame.fr>
2011-07-23 16:47:30 +0200

    Correct ClientCheck.

--- common/JackChannel.h
+++ common/JackChannel.h
@@ -74,7 +74,7 @@ class JackClientChannelInterface
             return -1;
         }
 
-        virtual void ClientCheck(const char* name, int uuid, char* name_res, int protocol, int options, int* status, int* result, int open = false)
+        virtual void ClientCheck(const char* name, int uuid, char* name_res, int protocol, int options, int* status, int* result, int open)
         {}
         virtual void ClientOpen(const char* name, int pid, int uuid, int* shared_engine, int* shared_client, int* shared_graph, int* result)
         {}
