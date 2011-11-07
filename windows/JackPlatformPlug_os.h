fad030616114f686f1181e6ba877f1e081a72cb4
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2011-11-07 12:30:09 +0000

    Define JACK_DEFAULT_DRIVER and JACK_LOCATION on Windows.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4572 0c269be4-1314-0410-8aa9-9f06e86f4224

--- windows/JackPlatformPlug_os.h
+++ windows/JackPlatformPlug_os.h
@@ -1,20 +1,20 @@
 /*
  Copyright (C) 2004-2008 Grame
- 
+
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation; either version 2.1 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- 
+
  */
 
 
@@ -24,6 +24,8 @@
 #define jack_server_dir "server"
 #define jack_client_dir "client"
 #define ADDON_DIR "jack"
+#define JACK_DEFAULT_DRIVER "portaudio"
+#define JACK_LOCATION "C:\Program Files\Jack v1.9.8"
 
 namespace Jack
 {
@@ -62,7 +64,7 @@ namespace Jack { typedef JackWinNamedPipe JackChannelTransaction; }
 #include "JackWinProcessSync.h"
 namespace Jack { typedef JackWinProcessSync JackProcessSync; }
 
-/* __JackPlatformServerChannel__ */ 
+/* __JackPlatformServerChannel__ */
 #include "JackWinNamedPipeServerChannel.h"
 namespace Jack { typedef JackWinNamedPipeServerChannel JackServerChannel; }
 
