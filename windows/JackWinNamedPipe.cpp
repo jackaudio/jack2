5257b1d1e145996c74b081d2f81df3f72e99ff64
sletz <sletz@0c269be4-1314-0410-8aa9-9f06e86f4224>
2012-01-05 09:55:49 +0000

    Restore old JackWinNamedPipeServer::AcceptClient.
    git-svn-id: http://subversion.jackaudio.org/jack/jack2/trunk/jackmp@4666 0c269be4-1314-0410-8aa9-9f06e86f4224

--- windows/JackWinNamedPipe.cpp
+++ windows/JackWinNamedPipe.cpp
@@ -303,18 +303,19 @@ bool JackWinNamedPipeServer::Accept()
 JackWinNamedPipeClient* JackWinNamedPipeServer::AcceptClient()
 {
     if (ConnectNamedPipe(fNamedPipe, NULL)) {
+        JackWinNamedPipeClient* client = new JackWinNamedPipeClient(fNamedPipe, fName);
         // Init the pipe to the default value
         fNamedPipe = INVALID_HANDLE_VALUE;
-        return new JackWinNamedPipeClient(fNamedPipe, fName);;
+        return client;
     } else {
-        jack_error("Cannot connect server pipe name = %s err = %ld", fName, GetLastError());
-        if (GetLastError() == ERROR_PIPE_CONNECTED) {
-            jack_error("Pipe already connnected = %s", fName);
-            // Init the pipe to the default value
-            fNamedPipe = INVALID_HANDLE_VALUE;
-            return new JackWinNamedPipeClient(fNamedPipe, fName);
-        } else {
-            return NULL;
+        switch (GetLastError()) {
+
+            case ERROR_PIPE_CONNECTED:
+                return new JackWinNamedPipeClient(fNamedPipe, fName);
+
+            default:
+                jack_error("Cannot connect server pipe name = %s  err = %ld", fName, GetLastError());
+                return NULL;
         }
     }
 }
