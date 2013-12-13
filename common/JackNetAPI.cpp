7123ef156273db039e12e7ff2df3a6008e765baf
Stephane Letz <letz@grame.fr>
2013-12-13 21:13:56 +0100

    Correct JackCoreMidiDriver.

--- common/JackNetAPI.cpp
+++ common/JackNetAPI.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2009-2011 Grame
+Copyright (C) 2009-2013 Grame
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
