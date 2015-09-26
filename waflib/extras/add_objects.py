a3ad1ff94bf83868b07d70d596fce76f8f57f334
Karl Linden <karl.j.linden@gmail.com>
2015-09-26 13:14:28 +0200

    Strip unnecessary tools and extras from waflib

--- waflib/extras/add_objects.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-# encoding: utf-8
-# Thomas Nagy, 2011 (ita)
-
-from waflib import Logs
-Logs.warn('This tool has been merged to the main library, remove the references to "add_objects"')
-
--- waflib/extras/sync_exec.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# encoding: utf-8
-
-"""
-This tool is obsolete, the sync_exec feature is now the default
-"""
-
-pass
-
