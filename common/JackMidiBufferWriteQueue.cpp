ec27b98632d7b39d48f21343c1ab90d7356530d7
clvn <clvn@users.noreply.github.com>
2017-12-29 20:39:34 +0100

    Added usings required by MSVC

--- common/JackMidiBufferWriteQueue.cpp
+++ common/JackMidiBufferWriteQueue.cpp
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include "JackMidiUtil.h"
 #include "JackError.h"
 
+using Jack::JackMidiBuffer;
 using Jack::JackMidiBufferWriteQueue;
 
 JackMidiBufferWriteQueue::JackMidiBufferWriteQueue()
