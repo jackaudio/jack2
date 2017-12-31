34a2f381e8af59d696c444d17fc6f9c5dd963446
clvn <clvn@users.noreply.github.com>
2017-12-31 03:24:32 +0100

    Windows: Fixed usleep() and inprocess dll exports in example clients

--- example-clients/showtime.c
+++ example-clients/showtime.c
@@ -28,6 +28,21 @@
 
 #ifdef _MSC_VER
 #define alloca _alloca
+
+#include <windows.h>
+
+void usleep(__int64 usec) 
+{ 
+    HANDLE timer; 
+    LARGE_INTEGER ft; 
+
+    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+    timer = CreateWaitableTimer(NULL, TRUE, NULL); 
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0); 
+    WaitForSingleObject(timer, INFINITE); 
+    CloseHandle(timer); 
+}
 #endif
 
 jack_client_t *client;
