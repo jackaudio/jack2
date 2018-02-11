a6700b7b05c56724b182681ff75de4bcef2221d0
houston <picotmathieu@gmail.com>
2018-02-11 10:35:57 +0100

    prevent client name to give uncorrect characters

--- linux/alsa/alsa_seqmidi.c
+++ linux/alsa/alsa_seqmidi.c
@@ -502,6 +502,10 @@ port_t* port_create(alsa_seqmidi_t *self, int type, snd_seq_addr_t addr, const s
   /* define the client name to use in port name and aliases */
 	snprintf(client_name , sizeof(client_name), snd_seq_client_info_get_name(client_info));
 	
+	for (c = client_name; *c; ++c)
+		if (!isalnum(*c) && *c != '/' && *c != '_' && *c != ':' && *c != '(' && *c != ')')
+			*c = '-';
+	
 	for (int i=1; i <= 16; i++){
 		snprintf(name, sizeof(name), "system:%s/midi_%s_%d", client_name, port_suffix, i);
 		
