d0e3749595ce2f90b0fd996c29eb5e825da7c7a0
Christoph Kuhr <christoph.kuhr@web.de>
2018-10-02 16:45:50 +0200

    added avb mediaclock backend

--- linux/avb/JackAVBDriver.cpp
+++ linux/avb/JackAVBDriver.cpp
@@ -16,7 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#include "JackAVBPDriver.h"
+#include "JackAVBDriver.h"
 #include "JackEngineControl.h"
 #include "JackLockedEngine.h"
 #include "JackGraphManager.h"
