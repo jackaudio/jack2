af30e6ac71098be78c23f39d0a61df16cfc5bce8
David Runge <dave@sleepmap.de>
2018-10-09 22:24:19 +0200

    .ci/install-deps.sh: Install script for dependencies.

---
+++ .ci/install-deps.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  brew install --c++11 \
+    pkg-config \
+    aften \
+    libsamplerate \
+    libsndfile \
+    opus \
+    readline \
+    doxygen
+fi
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  # autotools, automake, make are present in the trusty image
+  sudo apt-get install -y \
+    celt \
+    libffado-dev \
+    libsamplerate-dev \
+    libsndfile-dev \
+    libasound2-dev \
+    doxygen
+  # force installation of gcc-6 if required
+  if [ "${CC}" == "gcc-6" ]; then
+    sudo apt-get install gcc-6 g++-6
+  fi
+  # force installation of gcc-7 if required
+  if [ "${CC}" == "gcc-7" ]; then
+    sudo apt-get install gcc-7 g++-7
+  fi
+  # force installation of gcc-8 if required
+  if [ "${CC}" == "gcc-8" ]; then
+    sudo apt-get install gcc-8 g++-8
+  fi
+  # force installation of clang-3.8 if required
+  if [ "${CC}" == "clang-3.8" ]; then
+    sudo apt-get install clang-3.8
+  fi
+fi
+
+exit 0
