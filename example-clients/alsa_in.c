7cf849bf2e5e94054bf52fa47ca7542c8bad2e12
Timo Wischer <twischer@de.adit-jv.com>
2018-11-20 15:51:42 +0100

    alsa_in/out: Use float to S32 conversion if requested
    sample_move_d32u24_sS() converts into samples like 0x00****** but S32
    format expects samples like 0x********. Therefore it will not use the
    full volume range when also using sample_move_d32u24_sS() for S32.
    
    Change-Id: I509c135b0169020173309b5529c7d5b55dc292be
    Signed-off-by: Timo Wischer <twischer@de.adit-jv.com>

--- example-clients/alsa_in.c
+++ example-clients/alsa_in.c
@@ -95,9 +95,9 @@ typedef struct alsa_format {
 
 alsa_format_t formats[] = {
 	{ SND_PCM_FORMAT_FLOAT_LE, 4, sample_move_dS_floatLE, sample_move_floatLE_sSs, "float" },
-	{ SND_PCM_FORMAT_S32, 4, sample_move_d32u24_sS, sample_move_dS_s32u24, "32bit" },
+	{ SND_PCM_FORMAT_S32, 4, sample_move_d32_sS, sample_move_dS_s32, "32bit" },
 	{ SND_PCM_FORMAT_S24_3LE, 3, sample_move_d24_sS, sample_move_dS_s24, "24bit - real" },
-	{ SND_PCM_FORMAT_S24, 4, sample_move_d24_sS, sample_move_dS_s24, "24bit" },
+	{ SND_PCM_FORMAT_S24, 4, sample_move_d32u24_sS, sample_move_dS_s32u24, "24bit" },
 	{ SND_PCM_FORMAT_S16, 2, sample_move_d16_sS, sample_move_dS_s16, "16bit" }
 #ifdef __ANDROID__
 	,{ SND_PCM_FORMAT_S16_LE, 2, sample_move_d16_sS, sample_move_dS_s16, "16bit little-endian" }
