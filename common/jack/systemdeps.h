8c9d4b1fc2676f46e36c4972ffa6c798a810bdd6
justnope <dont@email.invalid>
2019-01-18 07:22:01 +0100

    changed  _WIN32 to WIN32

--- common/jack/systemdeps.h
+++ common/jack/systemdeps.h
@@ -133,7 +133,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #endif /* __arm__ || __aarch64__ || __ppc__ || __powerpc__ */
 
 /** define JACK_LIB_EXPORT, useful for internal clients */
-#if defined(_WIN32)
+#if defined(WIN32)
     #define JACK_LIB_EXPORT __declspec(dllexport)
 #elif defined(__GNUC__)
     #define JACK_LIB_EXPORT __attribute__((visibility("default")))
