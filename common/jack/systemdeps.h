6518ca28f78fa559a2f69271ae46832727b0a6b9
justnope <dont@email.invalid>
2019-02-04 07:06:07 +0100

    moved strcasecmp define to systemdeps.h

--- common/jack/systemdeps.h
+++ common/jack/systemdeps.h
@@ -58,6 +58,10 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #endif
 
+#ifdef _MSC_VER
+#define strcasecmp _stricmp
+#endif
+
 #if defined(_WIN32) && !defined(__CYGWIN__) && !defined(GNU_WIN32)
 
     #ifdef __MINGW32__
