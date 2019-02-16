c5d54f92404d9d46eb5f80253bbd57e50b642a22
Thomas Brand <tom@trellis.ch>
2019-02-16 13:55:58 +0100

    Add dependecies check to test_run.sh
    Running ldd -r -u <jack_binary> should tell about unused dependencies.
    
    Also see
    https://github.com/jackaudio/jack2/issues/430
    
    Minor changes
    -set script to fail on any error
    -check early for existing jackd
    -add comments

--- .ci/test_run.sh
+++ .ci/test_run.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+set -e
+
+#stop here if ./waf install wasn't successful
+ls -l /usr/bin/jackd
+
 echo "`date`"
 echo "$TRAVIS_OS_NAME"
 echo "========================================================================="
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+	#find installed files
 	sudo updatedb
 	locate jack | grep -e "/usr/bin" -e "/usr/lib" -e "/usr/share/man" -e "/usr/include"
-	ls -l /usr/bin/jackd
+	#show man pages
 	locate jack | grep /usr/share/man | grep "\.1" | while read line; do
 		man -P cat "$line"; done
+	#check for unused dependencies
+	ls -1 /usr/bin/jack_*|while read line; do
+		echo "checking unused dependencies for ${line}:"; ldd -r -u "$line"; done
 fi
 echo "========================================================================="
 jackd --version
-
-#echo "/usr/bin/jackd --verbose -p512 -t5000 -ddummy -r44100"> ~/.jackdrc
-#sudo jack_bufsize
-
 echo "========================================================================="
 sudo jackd -ddummy &
 ret=$!
@@ -25,14 +30,11 @@ sudo jack_lsp
 echo "========================================================================="
 sudo jack_bufsize
 echo "========================================================================="
-sudo jack_samplerate
+sudo jack_test #--verbose
 echo "========================================================================="
-sudo jack_test
-echo "========================================================================="
-#sudo jack_test --verbose
-#echo "========================================================================="
 echo "stopping jackd now"
 sudo kill -9 $ret
 echo "========================================================================="
 echo "`date`"
 echo "done"
+#EOF
