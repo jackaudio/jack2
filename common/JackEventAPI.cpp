e2c5a09a23dc311fa816179ec7db56b672e621d9
piegames <git@piegames.de>
2019-10-11 14:57:03 +0200

    Rename Midi API to Event API

--- common/JackEventAPI.cpp
+++ common/JackEventAPI.cpp
@@ -28,24 +28,24 @@ extern "C"
 {
 #endif
 
-    LIB_EXPORT uint32_t jack_midi_get_event_count(void* port_buffer);
+    LIB_EXPORT uint32_t jack_event_get_count(void* port_buffer);
 
-    LIB_EXPORT int jack_midi_event_get(jack_midi_event_t* event,
+    LIB_EXPORT int jack_event_get(jack_event_t* event,
                                    void* port_buffer, uint32_t event_index);
 
-    LIB_EXPORT void jack_midi_clear_buffer(void* port_buffer);
+    LIB_EXPORT void jack_event_clear_buffer(void* port_buffer);
     
-    LIB_EXPORT void jack_midi_reset_buffer(void* port_buffer);
+    LIB_EXPORT void jack_event_reset_buffer(void* port_buffer);
 
-    LIB_EXPORT size_t jack_midi_max_event_size(void* port_buffer);
+    LIB_EXPORT size_t jack_event_max_size(void* port_buffer);
 
-    LIB_EXPORT jack_midi_data_t* jack_midi_event_reserve(void* port_buffer,
+    LIB_EXPORT jack_event_data_t* jack_event_reserve(void* port_buffer,
             jack_nframes_t time, size_t data_size);
 
-    LIB_EXPORT int jack_midi_event_write(void* port_buffer,
-                                     jack_nframes_t time, const jack_midi_data_t* data, size_t data_size);
+    LIB_EXPORT int jack_event_write(void* port_buffer,
+                                     jack_nframes_t time, const jack_event_data_t* data, size_t data_size);
 
-    LIB_EXPORT jack_nframes_t jack_midi_get_lost_event_count(void* port_buffer);
+    LIB_EXPORT jack_nframes_t jack_event_get_lost_count(void* port_buffer);
 
 #ifdef __cplusplus
 }
@@ -54,9 +54,9 @@ extern "C"
 using namespace Jack;
 
 LIB_EXPORT
-uint32_t jack_midi_get_event_count(void* port_buffer)
+uint32_t jack_event_get_count(void* port_buffer)
 {
-    JackMidiBuffer *buf = (JackMidiBuffer*)port_buffer;
+    JackEventBuffer *buf = (JackEventBuffer*)port_buffer;
     if (!buf || !buf->IsValid()) {
         return 0;
     }
@@ -64,16 +64,16 @@ uint32_t jack_midi_get_event_count(void* port_buffer)
 }
 
 LIB_EXPORT
-int jack_midi_event_get(jack_midi_event_t *event, void* port_buffer, uint32_t event_index)
+int jack_event_get(jack_event_t *event, void* port_buffer, uint32_t event_index)
 {
-    JackMidiBuffer *buf = (JackMidiBuffer*)port_buffer;
+    JackEventBuffer *buf = (JackEventBuffer*)port_buffer;
     if (!buf || !buf->IsValid()) {
         return -EINVAL;
     }
     if (event_index >= buf->event_count) {
         return -ENOBUFS;
     }
-    JackMidiEvent* ev = &buf->events[event_index];
+    JackEvent* ev = &buf->events[event_index];
     event->time = ev->time;
     event->size = ev->size;
     event->buffer = ev->GetData(buf);
@@ -81,24 +81,24 @@ int jack_midi_event_get(jack_midi_event_t *event, void* port_buffer, uint32_t ev
 }
 
 LIB_EXPORT
-void jack_midi_clear_buffer(void* port_buffer)
+void jack_event_clear_buffer(void* port_buffer)
 {
-    JackMidiBuffer *buf = (JackMidiBuffer*)port_buffer;
+    JackEventBuffer *buf = (JackEventBuffer*)port_buffer;
     if (buf && buf->IsValid()) {
         buf->Reset(buf->nframes);
     }
 }
 
 LIB_EXPORT
-void jack_midi_reset_buffer(void* port_buffer)
+void jack_event_reset_buffer(void* port_buffer)
 {
-    MidiBufferInit(port_buffer, BUFFER_SIZE_MAX, BUFFER_SIZE_MAX);
+    EventBufferInit(port_buffer, BUFFER_SIZE_MAX, BUFFER_SIZE_MAX);
 }
 
 LIB_EXPORT
-size_t jack_midi_max_event_size(void* port_buffer)
+size_t jack_event_max_size(void* port_buffer)
 {
-    JackMidiBuffer *buf = (JackMidiBuffer*)port_buffer;
+    JackEventBuffer *buf = (JackEventBuffer*)port_buffer;
     if (buf && buf->IsValid()) {
         return buf->MaxEventSize();
     }
@@ -106,24 +106,24 @@ size_t jack_midi_max_event_size(void* port_buffer)
 }
 
 LIB_EXPORT
-jack_midi_data_t* jack_midi_event_reserve(void* port_buffer, jack_nframes_t time, size_t data_size)
+jack_event_data_t* jack_event_reserve(void* port_buffer, jack_nframes_t time, size_t data_size)
 {
-    JackMidiBuffer *buf = (JackMidiBuffer*)port_buffer;
+    JackEventBuffer *buf = (JackEventBuffer*)port_buffer;
     if (! buf) {
-        jack_error("jack_midi_event_reserve: port buffer is set to NULL");
+        jack_error("jack_event_reserve: port buffer is set to NULL");
         return 0;
     }
     if (! buf->IsValid()) {
-        jack_error("jack_midi_event_reserve: port buffer is invalid");
+        jack_error("jack_event_reserve: port buffer is invalid");
         return 0;
     }
     if (time >= buf->nframes) {
-        jack_error("jack_midi_event_reserve: time parameter is out of range "
+        jack_error("jack_event_reserve: time parameter is out of range "
                    "(%lu >= %lu)", time, buf->nframes);
         return 0;
     }
     if (buf->event_count && (buf->events[buf->event_count - 1].time > time)) {
-        jack_error("jack_midi_event_reserve: time parameter is earlier than "
+        jack_error("jack_event_reserve: time parameter is earlier than "
                    "last reserved event");
         return 0;
     }
@@ -131,17 +131,17 @@ jack_midi_data_t* jack_midi_event_reserve(void* port_buffer, jack_nframes_t time
 }
 
 LIB_EXPORT
-int jack_midi_event_write(void* port_buffer,
-                          jack_nframes_t time, const jack_midi_data_t* data, size_t data_size)
+int jack_event_write(void* port_buffer,
+                          jack_nframes_t time, const jack_event_data_t* data, size_t data_size)
 {
-    JackMidiBuffer *buf = (JackMidiBuffer*)port_buffer;
+    JackEventBuffer *buf = (JackEventBuffer*)port_buffer;
     if (!buf || !buf->IsValid()) {
         return -EINVAL;
     }
     if (time >= buf->nframes || (buf->event_count && buf->events[buf->event_count - 1].time > time)) {
         return -EINVAL;
     }
-    jack_midi_data_t* dest = buf->ReserveEvent(time, data_size);
+    jack_event_data_t* dest = buf->ReserveEvent(time, data_size);
     if (!dest) {
         return -ENOBUFS;
     }
@@ -150,9 +150,9 @@ int jack_midi_event_write(void* port_buffer,
 }
 
 LIB_EXPORT
-uint32_t jack_midi_get_lost_event_count(void* port_buffer)
+uint32_t jack_event_get_lost_count(void* port_buffer)
 {
-    JackMidiBuffer *buf = (JackMidiBuffer*)port_buffer;
+    JackEventBuffer *buf = (JackEventBuffer*)port_buffer;
     if (buf && buf->IsValid()) {
         return buf->lost_events;
     }
