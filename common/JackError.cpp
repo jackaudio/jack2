2980933a8003187ec217a4d9139fb232dcb3cd43
Florian Walpen <dev@submerge.ch>
2022-01-15 23:30:31 +0100

    API: Export missing symbols to jack library.
    The function pointers jack_error_callback and jack_info_callback are
    offered by the jack.h header as non-weak API members, but they were not
    exported to the jack library, only to the jackserver library.
    
    This broke the Jack backend of the OpenAl Soft library when switching
    from Jack1 to Jack2.

--- common/JackError.cpp
+++ common/JackError.cpp
@@ -133,5 +133,5 @@ SERVER_EXPORT void silent_jack_error_callback(const char *desc)
 SERVER_EXPORT void silent_jack_info_callback(const char *desc)
 {}
 
-SERVER_EXPORT void (*jack_error_callback)(const char *desc) = &default_jack_error_callback;
-SERVER_EXPORT void (*jack_info_callback)(const char *desc) = &default_jack_info_callback;
+LIB_EXPORT void (*jack_error_callback)(const char *desc) = &default_jack_error_callback;
+LIB_EXPORT void (*jack_info_callback)(const char *desc) = &default_jack_info_callback;
--- common/JackError.h
+++ common/JackError.h
@@ -35,8 +35,8 @@ extern "C"
     SERVER_EXPORT void jack_info(const char *fmt, ...);
     SERVER_EXPORT void jack_log(const char *fmt, ...);
 
-    SERVER_EXPORT extern void (*jack_error_callback)(const char *desc);
-    SERVER_EXPORT extern void (*jack_info_callback)(const char *desc);
+    LIB_EXPORT extern void (*jack_error_callback)(const char *desc);
+    LIB_EXPORT extern void (*jack_info_callback)(const char *desc);
 
     SERVER_EXPORT extern void default_jack_error_callback(const char *desc);
     SERVER_EXPORT extern void default_jack_info_callback(const char *desc);
