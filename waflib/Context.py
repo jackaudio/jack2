6b180d3f9e4247dfae7ca5fa03ca4d1964ac0275
Daan De Meyer <daan.j.demeyer@gmail.com>
2022-07-11 00:56:28 +0200

    Remove usage of 'U' mode bit for opening files in python
    The 'U' mode bit is removed in python 3.11. It has been
    deprecated for a long time. The 'U' mode bit has no effect
    so this change doesn't change any behavior.
    
    See https://docs.python.org/3.11/whatsnew/3.11.html#changes-in-the-python-api

--- waflib/Context.py
+++ waflib/Context.py
@@ -266,7 +266,7 @@ class Context(ctx):
 				cache[node] = True
 				self.pre_recurse(node)
 				try:
-					function_code = node.read('rU', encoding)
+					function_code = node.read('r', encoding)
 					exec(compile(function_code, node.abspath(), 'exec'), self.exec_dict)
 				finally:
 					self.post_recurse(node)
@@ -662,7 +662,7 @@ def load_module(path, encoding=None):
 
 	module = imp.new_module(WSCRIPT_FILE)
 	try:
-		code = Utils.readf(path, m='rU', encoding=encoding)
+		code = Utils.readf(path, m='r', encoding=encoding)
 	except EnvironmentError:
 		raise Errors.WafError('Could not read the file %r' % path)
 
